a78d993c13a72d05a7db2daa4402e3b42e7e3aae
Lucas Cassels <cassels@college.harvard.edu>
2018-02-10 11:01:35 -0500

    init_kalloc working minus memset bug

--- k-alloc.cc
+++ k-alloc.cc
@@ -6,25 +6,38 @@ static spinlock page_lock;
 static uintptr_t next_free_pa;
 
 // allocator constants
+static const int NPAGES = MEMSIZE_PHYSICAL / PAGESIZE;
 static const int MIN_ORDER = 12;
 static const int MAX_ORDER = 21;
 
+// physical page array
 struct pagestate {
     int order;
     bool allocated;
 };
-static pagestate pages[MEMSIZE_PHYSICAL / PAGESIZE];
+static pagestate pages[NPAGES];
 
+// memory block linked lists
 struct Block {
-    const int addr_;
+    const int pindex_;  // block location as pages array index
     list_links link_;
 
-    Block(const int addr)
-        : addr_(addr) {
+    Block(const int pindex)
+        : pindex_(pindex) {
     }
 };
 static list<Block, &Block::link_> _order_lists[MAX_ORDER - MIN_ORDER + 1];
-#define free_blocks(order) _order_lists[order - MIN_ORDER]
+
+// #define free_blocks(order)              \
+//     assert(order >= MIN_ORDER);         \
+//     assert(order <= MAX_ORDER);         \
+//     _order_lists[order - MIN_ORDER]
+
+list<Block, &Block::link_>* free_blocks(int order) {
+    assert(order >= MIN_ORDER);
+    assert(order <= MAX_ORDER);
+    return &_order_lists[order - MIN_ORDER];
+}
 
 x86_64_page* kallocpage() {
     auto irqs = page_lock.lock();
@@ -68,7 +81,7 @@ int find_max_order(uintptr_t start, uintptr_t end) {
 //    Initialize stuff needed by `kalloc`. Called from `init_hardware`,
 //    after `physical_ranges` is initialized.
 void init_kalloc() {
-    memset((void*) &pages, 0, sizeof(pages));
+    //memset(pages, 0, sizeof(pages));
 
     for (auto range = physical_ranges.begin();
      range != physical_ranges.end();
@@ -76,18 +89,24 @@ void init_kalloc() {
 
         auto addr = range->first();
         while(addr < range->last()) {
+            // find the largest buddy-allocation chunk we can make here
             int order = find_max_order(addr, range->last());
             assert(order > 0);
             pages[addr / PAGESIZE].order = order;
 
-            // is this range allocated?
+            // is the chunk allocated?
             if (range->type() == mem_available) {
                 pages[addr / PAGESIZE].allocated = false;
+
+                // add the available block to the free_blocks lists
+                Block new_block(addr / PAGESIZE);
+                free_blocks(order)->push_back(&new_block);
             }
             else {
                 pages[addr / PAGESIZE].allocated = true;
             }
 
+            // move past the chunk we just made
             addr += 1 << order;
         }
     }
@@ -110,11 +129,20 @@ void kfree(void* ptr) {
 // test_kalloc
 //    Run unit tests on the kalloc system.
 void test_kalloc() {
-    log_printf("Running kalloc unit tests...\n");
     // helper functions
     assert(find_max_order(7, 10) == -1);
     assert(find_max_order(0x1000, 0x10000) == 12);
     assert(find_max_order(0x10000, 0xFFFFFFFFFFF) == 16);
     assert(find_max_order(0x0, (uintptr_t) -1) == MAX_ORDER);
     assert(find_max_order(0x0, 0x1000) == 12);
+
+    // pages array invariants
+    // uintptr_t addr = 0;
+    // while (addr < MEMSIZE_PHYSICAL) {
+    //     auto page = &pages[addr / PAGESIZE];
+    //     assert(page->order >= MIN_ORDER);
+    //     assert(page->order <= MAX_ORDER);
+    //     assert(addr % (1 << page->order) == 0); // buddy allocator alignment
+    //     addr += (1 << page->order);
+    // }
 }
