2d63c100fa0077e0856656584a60186ee43532fa
Lucas Cassels <cassels@college.harvard.edu>
2018-02-11 14:41:19 -0500

    kfree (hopefully) working

--- k-alloc.cc
+++ k-alloc.cc
@@ -26,7 +26,7 @@ static list<pagestate, &pagestate::link_> _free_block_lists[NORDERS];
 
 // free_blocks(order)
 //    Returns a pointer to the linked list containing free blocks of size order
-list<pagestate, &pagestate::link_>* free_blocks(int order) {
+static list<pagestate, &pagestate::link_>* free_blocks(int order) {
     assert(order <= MAX_ORDER);
     assert(order >= MIN_ORDER);
     return &_free_block_lists[order - MIN_ORDER];
@@ -35,7 +35,7 @@ list<pagestate, &pagestate::link_>* free_blocks(int order) {
 
 // order_of(size)
 //    Returns the smallest order of size
-int order_of(int n) {
+static int order_of(int n) {
     if (n == 0) {
         return 0;
     }
@@ -75,7 +75,7 @@ x86_64_page* kallocpage() {
 //    Determine the largest order buddy-allocator block creatable at this
 //    address, considering available free space and alignment. Used in
 //    init_kalloc.
-int find_max_order(uintptr_t start, uintptr_t end) {
+static int find_max_order(uintptr_t start, uintptr_t end) {
     for (auto order = MAX_ORDER; order >= MIN_ORDER; order--) {
         if (start % (1 << order) == 0 && start + (1 << order) <= end) {
             return order;
@@ -86,7 +86,7 @@ int find_max_order(uintptr_t start, uintptr_t end) {
 
 
 // debug function
-void print_block_list(list<pagestate, &pagestate::link_>* lst) {
+static void print_block_list(list<pagestate, &pagestate::link_>* lst) {
     for (pagestate* b = lst->front(); b; b = lst->next(b)) {
         log_printf("%d ", b->pindex);
     }
@@ -132,7 +132,7 @@ void init_kalloc() {
 // min_larger_order
 //    Finds the smallest order block that can be broken up to eventually
 //    get blocks of goal order size
-int min_larger_order(int order) {
+static int min_larger_order(int order) {
     int test_order = order;
     while (free_blocks(test_order)->empty()) {
         ++test_order;
@@ -188,11 +188,74 @@ void* kalloc(size_t sz) {
     return pa2ka<void*>(free_block->pindex * PAGESIZE);
 }
 
+// buddy_pindex(p, order)
+//    Returns the pindex of the buddy block
+//    MUST BE CALLED WITH page_lock HELD
+static int buddy_pindex(int p, int order) {
+    p *= PAGESIZE;
+    if (p % (1 << (order + 1)) == 0) {
+        return (p + (1 << order)) / PAGESIZE;
+    }
+    else {
+        return (p - (1 << order)) / PAGESIZE;
+    }
+}
+
+
 // kfree(ptr)
 //    Free a pointer previously returned by `kalloc`, `kallocpage`, or
 //    `kalloc_pagetable`. Does nothing if `ptr == nullptr`.
 void kfree(void* ptr) {
-    assert(0 && "kfree not implemented yet");
+    if (ptr == nullptr) return;
+    assert(ka2pa(ptr) % PAGESIZE == 0);
+
+    auto irqs = page_lock.lock();
+    int pindex = ka2pa(ptr) / PAGESIZE;
+    assert(pages[pindex].allocated);
+
+    // free the memory
+    pages[pindex].allocated = false;
+    memset(ptr, 0, (1 << pages[pindex].order));
+
+    while (true) {
+        // find buddy address
+        int b_pindex = buddy_pindex(pindex, pages[pindex].order);
+
+        if (pages[b_pindex].allocated
+            || pages[b_pindex].order != pages[pindex].order
+            || pages[pindex].order == MAX_ORDER) {
+            break;
+        }
+        // buddy is not allocated and is the same order past here
+
+        auto to_merge = MAX(pindex, b_pindex);
+        auto merge_base = MIN(pindex, b_pindex);
+
+        // wipe merged block from existence and coalesce
+        free_blocks(pages[to_merge].order)->erase(&pages[to_merge]);
+        memset(&pages[to_merge], 0, sizeof(pagestate));
+        pages[merge_base].order++;
+
+        pindex = merge_base;
+    }
+
+    free_blocks(pages[pindex].order)->push_back(&pages[pindex]);
+    page_lock.unlock(irqs);
+}
+
+// check_pages_invariants
+//    Run through the pages array and check for broken invariants
+static void check_pages_invariants() {
+    // pages array invariants
+    uintptr_t addr = 0;
+    while (addr < MEMSIZE_PHYSICAL) {
+        auto page = &pages[addr / PAGESIZE];
+        assert(page->order >= MIN_ORDER);
+        assert(page->order <= MAX_ORDER);
+        assert(page->pindex == (int) (addr / PAGESIZE));
+        assert(addr % (1 << page->order) == 0); // buddy allocator alignment
+        addr += (1 << page->order);
+    }
 }
 
 // test_kalloc
@@ -205,19 +268,30 @@ void test_kalloc() {
     assert(find_max_order(0x0, (uintptr_t) -1) == MAX_ORDER);
     assert(find_max_order(0x0, 0x1000) == 12);
 
-    log_printf("msb(0) = 0x%x, order_of(0) = 0x%x\n", msb(0), order_of(0));
-    log_printf("msb(1) = 0x%x, order_of(1) = 0x%x\n", msb(1), order_of(1));
+    assert(order_of(0) == 0);
     assert(order_of(4095) == 12);
     assert(order_of(4096) == 12);
     assert(order_of(4097) == 13);
 
-    // pages array invariants
-    uintptr_t addr = 0;
-    while (addr < MEMSIZE_PHYSICAL) {
-        auto page = &pages[addr / PAGESIZE];
-        assert(page->order >= MIN_ORDER);
-        assert(page->order <= MAX_ORDER);
-        assert(addr % (1 << page->order) == 0); // buddy allocator alignment
-        addr += (1 << page->order);
+    assert(buddy_pindex(0x11000 / PAGESIZE, 12) == 0x10000 / PAGESIZE);
+    assert(buddy_pindex(0x10000 / PAGESIZE, 12) == 0x11000 / PAGESIZE);
+
+    // kalloc and kfree
+    assert(kalloc(0) == nullptr);
+    kfree(nullptr);
+    for (auto order = MIN_ORDER; order <= MAX_ORDER; order++) {
+        auto ptr = kalloc(1 << order);
+        check_pages_invariants();
+        if (ptr == nullptr) {
+            log_printf("WARNING: kalloc could not allocate order %d\n", order);
+            continue;
+        }
+        int pindex = ka2pa(ptr) / PAGESIZE;
+        assert(pages[pindex].allocated);
+        assert(pages[pindex].order == order);
+        assert(pages[pindex].pindex == pindex);
+        kfree(ptr);
+        assert(!pages[pindex].allocated);
+        check_pages_invariants();
     }
 }
