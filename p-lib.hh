449ee8c6f785512d77fc3436980297f43248f37a
Lucas <cassels@college.harvard.edu>
2018-05-03 11:40:10 -0400

    fixed p-lib function defs

--- p-lib.hh
+++ p-lib.hh
@@ -370,22 +370,22 @@ int printf(const char* format, ...);
 
 
 // Stubs for DOOM so we don't have to change source code if possible.
-void exit(int status) {
+static inline void exit(int status) {
     sys_exit(status);
 }
 
-uintptr_t malloc(int size) {
+static inline uintptr_t malloc(int size) {
     // TODO
     return 0x0;
 }
 
-int usleep(unsigned usec) {
+static inline int usleep(unsigned usec) {
     return sys_msleep(usec / 1000);
 }
 
 #define fprintf(fd, fmt, args...) dprintf(fd, fmt, ##args)
 
-int vfprintf(int fd, const char* format, va_list ap) {
+static inline int vfprintf(int fd, const char* format, va_list ap) {
     char buf[1025];
     size_t n = vsnprintf(buf, sizeof(buf), format, ap);
     if (n < sizeof(buf)) {
@@ -395,8 +395,8 @@ int vfprintf(int fd, const char* format, va_list ap) {
     }
 }
 
-int fopen(const char* path, const char* flags) {
-    sys_open(path, flags);
+static inline int fopen(const char* path, int flags) {
+    return sys_open(path, flags);
 }
 
 
